856896a47048166e94bc3acea28bba81087ad10a
Haimanot <haimanotgetu18@gmail.com>
2020-10-01 15:16:12 -0600

    Made some changes to the amount of formset endpoints receive

---
+++ models/StudentGroup.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const studentGroupSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    semester: {
+        type: ObjectId,
+        ref: 'Semester',
+    }
+})
+
+module.exports = mongoose.model('StudentGroup',studentGroupSchema);
